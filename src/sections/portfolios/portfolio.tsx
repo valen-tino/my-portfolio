3ec3091932f6fa8f64ad5dc669c4497ff2c25645
valen-tino <valentinojehaut@gmail.com>
2023-04-05 19:43:10 +0800

    1st commit

--- src/sections/portfolios/portfolio.tsx
+++ src/sections/portfolios/portfolio.tsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
   return (
     <section id='portfolio'>
         <h1 className="mb-10 text-5xl md:text-8xl font-bold underline underline-offset-8 decoration-wavy text-gray-100 decoration-red-400">My Portfolios</h1>
-        <h2 className='text-md md:text-2xl mb-10 font-semibold'>Here's some of my portfolios!</h2>
+        <h2 className='text-md md:text-2xl mb-10 font-semibold'>Here's some of my portfolios!!</h2>
         <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">            
             <PortfolioCard/>
         </div>
