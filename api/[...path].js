cd6b4b04ed039d315c8417753935f6e3f25dd9b4
valen-tino <59046897+valen-tino@users.noreply.github.com>
2025-09-07 12:20:27 +0800

    Export explicit handlers in api/[...path].js and api/index.js to forward to Express app

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,7 @@
 // Catch-all API function that forwards all /api/* routes to the Express app
 const app = require('./serverless');
 
-module.exports = app;
+module.exports = (req, res) => {
+  return app(req, res);
+};
 
--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 // This file serves as the main entry point for Vercel Serverless Functions
 const app = require('./serverless');
 
-// Export the Express app for Vercel
-module.exports = app;
+// Export a handler function that forwards to Express
+module.exports = (req, res) => {
+  return app(req, res);
+};
